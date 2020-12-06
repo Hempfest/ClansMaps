f52ab35ac60e2031408c5a01857222f84ddf043f
Hempfest <64885225+Hempfest@users.noreply.github.com>
2020-12-05 18:58:57 -0800

    Accommodating clans 2.0.8-R2

--- src/main/java/com/youtube/hempfest/dynmap/ClansMaps.java
+++ src/main/java/com/youtube/hempfest/dynmap/ClansMaps.java
@@ -8,7 +8,11 @@ import com.youtube.hempfest.clans.util.construct.ClanUtil;
 import com.youtube.hempfest.clans.util.data.Config;
 import com.youtube.hempfest.clans.util.data.ConfigType;
 import com.youtube.hempfest.clans.util.data.DataManager;
+import com.youtube.hempfest.clans.util.events.SubCommandEvent;
+import com.youtube.hempfest.clans.util.events.TabInsertEvent;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import org.bukkit.Bukkit;
@@ -17,10 +21,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dynmap.markers.AreaMarker;
-import org.spigotmc.hessentials.listener.events.hempfest.WarpGateEvent;
 
 public final class ClansMaps extends JavaPlugin implements Listener {
 
@@ -48,14 +50,135 @@ public final class ClansMaps extends JavaPlugin implements Listener {
 		return Clan.clanUtil;
 	}
 
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onWarpGate(WarpGateEvent e) {
-		if (Claim.claimUtil.isInClaim(e.getGateBlock().getLocation())) {
-			e.setGateMessage("Youve been poofed mate!! POOFED!!");
+	@EventHandler
+	public void onTab(TabInsertEvent e) {
+		List<String> add = new ArrayList<>(Arrays.asList("map", "unmap"));
+		for (String a : add) {
+			if (!e.getArgs(1).contains(a)) {
+				e.add(1, a);
+			}
 		}
-
 	}
 
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onClanCommand(SubCommandEvent e) {
+		Player p = e.getSender();
+		int length = e.getArgs().length;
+		String[] args = e.getArgs();
+		StringLibrary lib = new StringLibrary();
+		if (length == 1) {
+			if (args[0].equalsIgnoreCase("map")) {
+				if (Clan.clanUtil.getClan(p) != null) {
+					Clan clan = HempfestClans.clanManager(p);
+					lib.sendMessage(p, "&e&oUpdating dynmap with claim information..");
+					if (Arrays.asList(clan.getOwnedClaims()).size() == 0) {
+						lib.sendMessage(p, "&c&oClaim mapping task failed. No claims to map.");
+						e.setReturn(true);
+					}
+					if (integration.getFailedAttempt() != null) {
+						lib.sendMessage(p, integration.getFailedAttempt());
+					}
+					if (getUtil().getRankPower(p) >= 2) {
+						long time = System.currentTimeMillis();
+						integration.fillMap(clan.getOwnedClaims());
+						long complete = (System.currentTimeMillis() - time) / 1000;
+						int second = Integer.parseInt(String.valueOf(complete));
+						lib.sendMessage(p, "&a&oClaim mapping task completed in &f" + second + "&a&os");
+						HempfestClans.getInstance().getLogger().info("- (" + clan.getClanTag() + ") Marker sets successfully updated in accordance to claims.");
+					} else {
+						lib.sendMessage(p, "&c&oYou do not have clan clearance.");
+						e.setReturn(true);
+					}
+				}
+				e.setReturn(true);
+			}
+			if (args[0].equalsIgnoreCase("unmap")) {
+				if (Clan.clanUtil.getClan(p) != null) {
+					Clan clan = HempfestClans.clanManager(p);
+					if (Claim.claimUtil.isInClaim(p.getLocation())) {
+						Claim claim = new Claim(Claim.claimUtil.getClaimID(p.getLocation()));
+						if (Arrays.asList(clan.getOwnedClaims()).contains(claim.getClaimID())) {
+							Set<AreaMarker> markers = integration.markerset.getAreaMarkers();
+							if (getUtil().getRankPower(p) >= 2) {
+								for (AreaMarker am : markers) {
+									if (am.getMarkerID().equals(claim.getClaimID())) {
+										am.deleteMarker();
+										lib.sendMessage(p, "&b&oCurrent claim visibility has been removed from the map.");
+										e.setReturn(true);
+									}
+								}
+							} else {
+								lib.sendMessage(p, "&c&oYou do not have clan clearance.");
+								e.setReturn(true);
+							}
+							e.setReturn(true);
+						}
+						lib.sendMessage(p, lib.notClaimOwner(claim.getOwner()));
+					} else {
+						lib.sendMessage(p, "This land belongs to: &4&nWilderness&r, and is free to claim.");
+						e.setReturn(true);
+					}
+				} else {
+					lib.sendMessage(p, lib.notInClan());
+					e.setReturn(true);
+				}
+			}
+			if (args[0].equalsIgnoreCase("unclaim")) {
+				if (Clan.clanUtil.getClan(p) != null) {
+					if (getUtil().getRankPower(p) >= getUtil().claimingClearance()) {
+						Clan clan = HempfestClans.clanManager(p);
+						if (Claim.claimUtil.isInClaim(p.getLocation())) {
+							if (Arrays.asList(clan.getOwnedClaims()).contains(Claim.claimUtil.getClaimID(p.getLocation()))) {
+								integration.removeMarker(Claim.claimUtil.getClaimID(p.getLocation()));
+							} else {
+								if (getUtil().shieldStatus()) {
+									if (getUtil().overPowerBypass()) {
+										Claim claim = new Claim(Claim.claimUtil.getClaimID(p.getLocation()));
+										Clan clan2 = claim.getClan();
+										if (clan.getPower() > clan2.getPower()) {
+											integration.removeMarker(claim.getClaimID());
+											e.setReturn(true);
+										}
+									}
+								} else {
+									Claim claim = new Claim(Claim.claimUtil.getClaimID(p.getLocation()));
+									Clan clan2 = new Clan(claim.getOwner());
+									if (clan.getPower() > clan2.getPower()) {
+										integration.removeMarker(claim.getClaimID());
+										e.setReturn(true);
+									}
+								}
+							}
+						}
+					}
+				}
+
+			}
+		}
+		if (length == 2) {
+			if (args[0].equalsIgnoreCase("unclaim")) {
+				if (args[1].equalsIgnoreCase("all")) {
+					DataManager dm = new DataManager("Regions", "Configuration");
+					Config regions = dm.getFile(ConfigType.MISC_FILE);
+					FileConfiguration d = regions.getConfig();
+					if (Clan.clanUtil.getClan(p) != null) {
+						if (getUtil().getRankPower(p) >= getUtil().unclaimAllClearance()) {
+							if (!d.isConfigurationSection(getUtil().getClan(p) + ".Claims")) {
+								e.setReturn(false);
+							}
+							if (!Objects.requireNonNull(d.getConfigurationSection(getUtil().getClan(p) + ".Claims")).getKeys(false).isEmpty()) {
+								for (String claimID : d.getConfigurationSection(getUtil().getClan(p) + ".Claims").getKeys(false)) {
+									integration.removeMarker(claimID);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/*
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onCommand(PlayerCommandPreprocessEvent e) {
 		Player p = e.getPlayer();
@@ -66,127 +189,11 @@ public final class ClansMaps extends JavaPlugin implements Listener {
 		int length = args.length;
 		StringLibrary lib = new StringLibrary();
 		if (label.equalsIgnoreCase("clan") || label.equalsIgnoreCase("c")) {
-		if (length == 1) {
-				if (args[0].equalsIgnoreCase("map")) {
-					if (Clan.clanUtil.getClan(p) != null) {
-						Clan clan = HempfestClans.clanManager(p);
-						lib.sendMessage(p, "&e&oUpdating dynmap with claim information..");
-						if (Arrays.asList(clan.getOwnedClaims()).size() == 0) {
-							lib.sendMessage(p, "&c&oClaim mapping task failed. No claims to map.");
-							e.setCancelled(true);
-							return;
-						}
-						if (integration.getFailedAttempt() != null) {
-							lib.sendMessage(p, integration.getFailedAttempt());
-						}
-						if (getUtil().getRankPower(p) >= 2) {
-							long time = System.currentTimeMillis();
-							integration.fillMap(clan.getOwnedClaims());
-							long complete = (System.currentTimeMillis() - time) / 1000;
-							int second = Integer.parseInt(String.valueOf(complete));
-							lib.sendMessage(p, "&a&oClaim mapping task completed in &f" + second + "&a&os");
-							HempfestClans.getInstance().getLogger().info("- (" + clan.getClanTag() + ") Marker sets successfully updated in accordance to claims.");
-						} else {
-							lib.sendMessage(p, "&c&oYou do not have clan clearance.");
-							e.setCancelled(true);
-							return;
-						}
-					}
-					e.setCancelled(true);
-					return;
-				}
-				if (args[0].equalsIgnoreCase("unmap")) {
-					if (Clan.clanUtil.getClan(p) != null) {
-						Clan clan = HempfestClans.clanManager(p);
-						if (Claim.claimUtil.isInClaim(p.getLocation())) {
-							Claim claim = new Claim(Claim.claimUtil.getClaimID(p.getLocation()));
-							if (Arrays.asList(clan.getOwnedClaims()).contains(claim.getClaimID())) {
-								Set<AreaMarker> markers = integration.markerset.getAreaMarkers();
-								if (getUtil().getRankPower(p) >= 2) {
-									for (AreaMarker am : markers) {
-										if (am.getMarkerID().equals(claim.getClaimID())) {
-											am.deleteMarker();
-											lib.sendMessage(p, "&b&oCurrent claim visibility has been removed from the map.");
-											e.setCancelled(true);
-											return;
-										}
-									}
-								} else {
-									lib.sendMessage(p, "&c&oYou do not have clan clearance.");
-									e.setCancelled(true);
-									return;
-								}
-								e.setCancelled(true);
-								return;
-							}
-							lib.sendMessage(p, lib.notClaimOwner(claim.getOwner()));
-						} else {
-							lib.sendMessage(p, "This land belongs to: &4&nWilderness&r, and is free to claim.");
-							e.setCancelled(true);
-							return;
-						}
-					} else {
-						lib.sendMessage(p, lib.notInClan());
-						e.setCancelled(true);
-						return;
-					}
-					e.setCancelled(true);
-					return;
-				}
-				if (args[0].equalsIgnoreCase("unclaim")) {
-					if (Clan.clanUtil.getClan(p) != null) {
-						if (getUtil().getRankPower(p) >= getUtil().claimingClearance()) {
-							Clan clan = HempfestClans.clanManager(p);
-							if (Claim.claimUtil.isInClaim(p.getLocation())) {
-								if (Arrays.asList(clan.getOwnedClaims()).contains(Claim.claimUtil.getClaimID(p.getLocation()))) {
-									integration.removeMarker(Claim.claimUtil.getClaimID(p.getLocation()));
-								} else {
-									if (getUtil().shieldStatus()) {
-										if (getUtil().overPowerBypass()) {
-											Claim claim = new Claim(Claim.claimUtil.getClaimID(p.getLocation()));
-											Clan clan2 = claim.getClan();
-											if (clan.getPower() > clan2.getPower()) {
-												integration.removeMarker(claim.getClaimID());
-											}
-										}
-									} else {
-										Claim claim = new Claim(Claim.claimUtil.getClaimID(p.getLocation()));
-										Clan clan2 = new Clan(claim.getOwner());
-										if (clan.getPower() > clan2.getPower()) {
-											integration.removeMarker(claim.getClaimID());
-										}
-									}
-								}
-							}
-						}
-					}
-					return;
-				}
-			}
-		if (length == 2) {
-				if (args[0].equalsIgnoreCase("unclaim")) {
-					if (args[1].equalsIgnoreCase("all")) {
-						DataManager dm = new DataManager("Regions", "Configuration");
-						Config regions = dm.getFile(ConfigType.MISC_FILE);
-						FileConfiguration d = regions.getConfig();
-						if (Clan.clanUtil.getClan(p) != null) {
-							if (getUtil().getRankPower(p) >= getUtil().unclaimAllClearance()) {
-								if (!d.isConfigurationSection(getUtil().getClan(p) + ".Claims")) {
-									return;
-								}
-								if (!Objects.requireNonNull(d.getConfigurationSection(getUtil().getClan(p) + ".Claims")).getKeys(false).isEmpty()) {
-									for (String claimID : d.getConfigurationSection(getUtil().getClan(p) + ".Claims").getKeys(false)) {
-										integration.removeMarker(claimID);
-										return;
-									}
-								}
-							}
-						}
-					}
-				}
-		}
+
 		}
 	}
+
+	 */
 
 
 }
